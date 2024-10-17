4673fc994074d0710f2070be7c90718b73859a93
Mostafa Gamal <19362671+twiki2@users.noreply.github.com>
2024-10-17 21:01:49 +0300

    Update server.c

--- server.c
+++ server.c
@@ -7,24 +7,24 @@
 
 #define PORT 8080
 
-int main(){
+int main() {
 	
-	int skt = socket(AF_INET, SOCK_STREAM, 0);
+   int skt = socket(AF_INET, SOCK_STREAM, 0);
 	
-	struct sockaddr_in host_address;
+    struct sockaddr_in host_address;
     host_address.sin_family = AF_INET;
     host_address.sin_port = htons(PORT);
     host_address.sin_addr.s_addr = htonl(INADDR_ANY); 
 	
-	bind(skt, (struct sockaddr*)&host_address, sizeof(host_address) );
+    bind(skt, (struct sockaddr*)&host_address, sizeof(host_address) );
 	
-	listen(skt, 10);
+    listen(skt, 10);
 	
-	int acptsocket = accept(skt, 0, 0);
+    int acptsocket = accept(skt, 0, 0);
 	
-	char buffer[256] = {0};
+    char buffer[256] = {0};
 	
-	recv(acptsocket, buffer, 255, 0);
+    recv(acptsocket, buffer, 255, 0);
 	
 	
     const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
@@ -34,4 +34,6 @@ int main(){
     close(acptsocket);
     close(skt);
 	
-	return 0;}
+    return 0;
+
+}
